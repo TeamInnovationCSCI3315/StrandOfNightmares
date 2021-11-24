d27b260dd1f7a16a3de00b2271fe9f770ba2f0cf
andrewmester1 <75629301+andrewmester1@users.noreply.github.com>
2021-11-24 17:58:27 -0500

    Added Test cases for changing player location

--- StrandOfNightmares1/TeamInnovationUnitTests/TeamInnovationUnitTests.cpp
+++ StrandOfNightmares1/TeamInnovationUnitTests/TeamInnovationUnitTests.cpp
@@ -1,6 +1,12 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 #include "../Inventory.cpp"
+#include "../Locations.cpp"
+#include "../Locations.h"
+#include "../Inventory.h"
+#include "../GameClass.cpp"
+#include "../PlayerClass.cpp"
+#include "../InputValidation.cpp"
 #include <string>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
@@ -81,6 +87,26 @@ namespace TeamInnovationUnitTests
 			Assert::IsTrue(playerInventory.SearchInventory("&%$$&"));
 			Assert::IsTrue(playerInventory.SearchInventory("CTRL+C"));
 		}
+		/*
+		Test if player changes locations. If they go west, they should be moved to Room, if they select North, they should be moved to Room 2 after going to Room
+		*/
+		TEST_METHOD(TestPlayerLocationChange)
+		{
+			Locations PlayerLocation("Village", "", "", "", "", "Room", "", "");
+			Locations Room("Room", "", "Room 2", "", "Village", "", "", "");
+			Locations Room2("Room 2", "", "Village", "", "", "", "", "");
+			Locations AllLocations[2] = { Room,Room2 };
+			PlayerLocation.CheckLocation(AllLocations, "West", 2);
+			string room = "Room";
+			Assert::AreEqual(PlayerLocation.getLocationName(), room);
+			room = "Village";
+			Assert::AreNotEqual(PlayerLocation.getLocationName(), room);
+			PlayerLocation.CheckLocation(AllLocations, "North", 2);
+			room = "Room 2";
+			Assert::AreEqual(PlayerLocation.getLocationName(), room);
+			room = "Room 3";
+			Assert::AreNotEqual(PlayerLocation.getLocationName(), room);
 
+		}
 	};
 }
